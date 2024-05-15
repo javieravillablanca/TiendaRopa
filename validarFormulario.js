5706eaa8c536979ce7adf33f311be17ea796b74c
Javii <javi.villablanca@duocuc.cl>
2024-05-15 01:37:30 -0400

    mi tercer commit del proyecto

--- validarFormulario.js
+++ validarFormulario.js
@@ -7,23 +7,25 @@ const errorNombre = document.getElementById('errorNombre');
 const errorApellido = document.getElementById('errorApellido'); 
 const errorNumero = document.getElementById('errorNumero'); 
 const errorEmail = document.getElementById('errorEmail'); 
-  
-  
-  
-  
+
+nombre.addEventListener('input', validarFormularioNombre);
+lastName.addEventListener('input', validarFormularioApellido);
+number.addEventListener('input', validarFormularioNumero);
+email.addEventListener('input', validarFormularioEmail);
+
+
 function validarFormularioNombre() {
     let mensajesError = [];
     
     if (nombre.value === null || nombre.value === '') {
-        mensajesError.push('¡Ingrese su nombre!');
-    } else if (nombre.value.length < 3) {
-        mensajesError.push('El nombre debe contener al menos 3 letras');
+        mensajesError.push(' ¡Ingrese su nombre!. ');
+
     } else if (/\d/.test(nombre.value)) {
         nombre.value = nombre.value.replace(/\d/g, '');
-        mensajesError.push('El nombre debe contener solo letras');
-    }
-    if (/[0-9]/.test(nombre.value)) {
-        mensajesError.push('Debe ingresar solo letras');
+        mensajesError.push(' Ingresar solo letras, por favor. ');
+
+    } else if (nombre.value.length < 3) {
+        mensajesError.push(' Debe contener al menos 3 letras. ');
     }
     if (mensajesError.length > 0) {
         errorNombre.textContent = mensajesError.join(' ');
@@ -36,16 +38,13 @@ function validarFormularioApellido() {
     let mensajesError = [];
     
     if (lastName.value === null || lastName.value === '') {
-        mensajesError.push('¡Ingrese su Apellido!');
-    } else if (lastName.value.length < 3) {
-        mensajesError.push('El apellido debe contener al menos 3 letras');
+        mensajesError.push(' ¡Ingrese su Apellido!.');
     } else if (/\d/.test(lastName.value)) {
         lastName.value = lastName.value.replace(/\d/g, '');
-        mensajesError.push('El apellido debe contener solo letras');
-    }
-    if (/[0-9]/.test(lastName.value)) {
-        mensajesError.push('Debe ingresar solo letras');
-    }
+        mensajesError.push(' Ingresar solo letras, por favor.');
+    } else if (lastName.value.length < 3) {
+        mensajesError.push(' Debe contener al menos 3 letras.');
+    } 
     if (mensajesError.length > 0) {
         errorApellido.textContent = mensajesError.join(' ');
     } else {
@@ -58,15 +57,24 @@ function validarFormularioApellido() {
 function validarFormularioNumero() {
     let mensajesError = [];
 
+    
+
         if(number.value === null || number.value === ''){
-         mensajesError.push('¡Ingrese su numero!');
-         errorNumero.textContent = '¡Ingrese su numero!';
-       }else if (/[A-Z a-z]/.test(number.value))  {
-         mensajesError.push('Debe ingresar solo numeros');
-         errorNumero.textContent = 'Debe ingresar solo numeros';
-  
-       }else{
-          errorNumero.textContent='';
+         mensajesError.push(' ¡Ingrese su numero!.');
+         errorNumero.textContent = ' ¡Ingrese su numero!.';  
+        }else if (/[A-Z a-z]/.test(number.value))  {
+        number.value = number.value.replace(/\D/g, '');
+         mensajesError.push(' Ingresar solo numeros, por favor.');
+         errorNumero.textContent = ' Ingresar solo numeros, por favor.';
+        } else if (!/^\d{5,}$/.test(number.value)) {
+            
+            mensajesError.push(' Debe ingresar al menos 5 numeros.');
+            errorNumero.textContent = ' Debe ingresar al menos 5 numeros.';
+        }
+        if (mensajesError.length > 0) {
+            errorNumero.textContent = mensajesError.join(' ');
+        } else {
+            errorNumero.textContent = '';
         }
   
 }   
@@ -76,11 +84,14 @@ function validarFormularioEmail() {
     let mensajesError = [];
 
         if (email.value === null || email.value === '') {
-         mensajesError.push('¡Ingrese su email!');
-         errorEmail.textContent = '¡Ingrese su email!';
+         mensajesError.push(' ¡Ingrese su email!.');
+         errorEmail.textContent = '¡Ingrese su email!.';
         } else if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value)) {
-         mensajesError.push('El email no tiene un formato valido');
-         errorEmail.textContent = 'El email no tiene un formato valido';
+         mensajesError.push(' El email no tiene un formato valido');
+         errorEmail.textContent = ' El email no tiene un formato valido.';
+        
+
+
         } else {
          errorEmail.textContent = ''; 
         }
